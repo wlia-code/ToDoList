16b249c4b9e8b80e721a85144cb6f3a60ca5f258
wlia-code <wassimrawwas@gmail.com>
2024-03-23 23:10:05 +0000

    add edit_ctegory to update task

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -10,14 +10,25 @@ def home():
 
 @app.route("/categories")
 def categories():
-    return render_template("categories.html")
+    categories = list(Category.query.order_by(Category.category_name).all())
+    return render_template("categories.html", categories=categories)
 
 
 @app.route("/add_category", methods=["GET", "POST"])
 def add_category():
-    if request.methods == "POST":
-        category = category(category_name=request.form.get("category_name"))
+    if request.method == "POST":
+        category = Category(category_name=request.form.get("category_name"))
         db.session.add(category)
         db.session.commit()
-        return redirect(url_for("catgories"))
+        return redirect(url_for("categories"))
     return render_template("add_category.html")
+
+
+@app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
+def edit_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    if request.method == "POST":
+        category.category_name = request.form.get("category_name")
+        db.session.commit()
+        return redirect(url_for("categories"))
+    return render_template("edit_category.html", category=category)
