617fc203a30f9e4ff1ac6829a249026e1e1d1dc0
wlia-code <wassimrawwas@gmail.com>
2024-03-26 21:14:51 +0000

    add postgres and procfile

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -5,8 +5,18 @@ if os.path.exists("env.py"):
     import env  # noqa
 
 
-app = Flask(__name__)
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+ app = Flask(__name__)
+ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+
+ if os.environ.get("DEVELOPMENT") == "True":
+     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+ else:
+     uri = os.environ.get("DATABASE_URL")
+     if uri.startswith("postgres://"):
+         uri = uri.replace("postgres://", "postgresql://", 1)
+     app.config["SQLALCHEMY_DATABASE_URI"] = uri
+
+
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
 db = SQLAlchemy(app)
